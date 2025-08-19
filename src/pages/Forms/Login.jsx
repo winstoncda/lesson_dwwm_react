3e550b7d266efbcda4eaa0a990563d283cdbcbff
winstoncda <olivier.pro.cda@gmail.com>
2025-08-19 11:02:09 +0200

    mise en place du système de connexion et inscription

--- src/pages/Forms/Login.jsx
+++ src/pages/Forms/Login.jsx
@@ -1,3 +1,74 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { NavLink } from "react-router-dom";
+
 export default function Login() {
-  return <div>Login</div>;
+  const defaultValues = {
+    data: "",
+    password: "",
+  };
+
+  const schema = yup.object({
+    data: yup.string().required("Ce champ est obligatoire"),
+    password: yup.string().required("Le mot de passe est obligatoire"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues,
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  function submit(values) {
+    console.log(values);
+    reset(defaultValues);
+    // requete HTTP
+  }
+  return (
+    <div className="w-full max-w-md p-6 bg-white shadow-xl rounded">
+      <form
+        className="flex flex-col gap-4 mb-6 mx-auto max-w-[400px]"
+        onSubmit={handleSubmit(submit)}
+      >
+        <div className="flex flex-col mb-2">
+          <label htmlFor="data" className="mb-2">
+            Pseudo ou Email
+          </label>
+          <input
+            {...register("data")}
+            type="text"
+            id="data"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.data && <p className="text-red-500">{errors.data.message}</p>}
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="password" className="mb-2">
+            Mot de passe
+          </label>
+          <input
+            {...register("password")}
+            type="password"
+            id="password"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+        </div>
+        <NavLink to="/register" className="text-blue-500">
+          Pas encore inscrit ?
+        </NavLink>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Forms/Register.jsx
+++ src/pages/Forms/Register.jsx
@@ -1,3 +1,138 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { NavLink } from "react-router-dom";
+
 export default function Register() {
-  return <div>Register</div>;
+  const defaultValues = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    rgpd: false,
+  };
+
+  const schema = yup.object({
+    username: yup.string().required("Ce champ est obligatoire"),
+    email: yup
+      .string()
+      .email()
+      .required("Le champ est obligatoire")
+      .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g, "Format email non valide"),
+    password: yup
+      .string()
+      .required("Le mot de passe est obligatoire")
+      .min(5, "Trop court")
+      .max(10, "trop long"),
+    confirmPassword: yup
+      .string()
+      .required("La confirmation de mot de passe est obligatoire")
+      .oneOf(
+        [yup.ref("password"), ""],
+        "Les mots de passe ne correspondent pas"
+      ),
+    rgpd: yup
+      .boolean()
+      .oneOf([true], "Vous devez accepter les termes et conditions"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues,
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  function submit(values) {
+    console.log(values);
+    reset(defaultValues);
+    // requete HTTP
+  }
+  return (
+    <div className="w-full max-w-md p-6 bg-white shadow-xl rounded">
+      <form
+        className="flex flex-col gap-4 mb-6 mx-auto max-w-[400px]"
+        onSubmit={handleSubmit(submit)}
+      >
+        <div className="flex flex-col mb-2">
+          <label htmlFor="username" className="mb-2">
+            Pseudo
+          </label>
+          <input
+            {...register("username")}
+            type="text"
+            id="username"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.username && (
+            <p className="text-red-500">{errors.username.message}</p>
+          )}
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="email" className="mb-2">
+            Email
+          </label>
+          <input
+            {...register("email")}
+            type="email"
+            id="email"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="password" className="mb-2">
+            Mot de passe
+          </label>
+          <input
+            {...register("password")}
+            type="password"
+            id="password"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="confirmPassword" className="mb-2">
+            Confirmation du mot de passe
+          </label>
+          <input
+            {...register("confirmPassword")}
+            type="password"
+            id="confirmPassword"
+            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.confirmPassword && (
+            <p className="text-red-500">{errors.confirmPassword.message}</p>
+          )}
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="rgpd" className="mb-2">
+            <input
+              {...register("rgpd")}
+              type="checkbox"
+              className="mr-4"
+              id="rgpd"
+            />
+            En soumettant ce formulaire, j'accepte ...
+          </label>
+          {errors.rgpd && <p className="text-red-500">{errors.rgpd.message}</p>}
+        </div>
+        <NavLink to="/login" className="text-blue-500">
+          Déjà inscrit ?
+        </NavLink>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
