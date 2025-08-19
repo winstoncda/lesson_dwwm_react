0e82a4da0d9a8d89afce546ae633112bb5497653
winstoncda <olivier.pro.cda@gmail.com>
2025-08-19 13:34:21 +0200

    ajout constantes css et utilisation Button

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -1,3 +1,9 @@
-export default function Button() {
-  return <button>Click Me</button>;
+import { variants } from "../../constants";
+
+export default function Button({ color, text }) {
+  return (
+    <button className={`w-[200px] px-4 py-2 rounded-2xl ${variants[color]}`}>
+      {text}
+    </button>
+  );
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,3 +1,10 @@
+import Button from "../../components/Common/Button";
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Button color="primary" text="S'abonner" />
+      <Button color="secondary" text="Valider" />
+    </div>
+  );
 }
