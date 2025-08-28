c800908994d30fce7ded8a16a102ade34b5b9387
winstoncda <olivier.pro.cda@gmail.com>
2025-08-28 11:14:57 +0200

    ajout d'une boucle et d'un composant card

---
+++ src/components/Common/Card.jsx
@@ -0,0 +1,17 @@
+export default function Card({ blog }) {
+  return (
+    <div className="border border-gray-400 rounded-lg m-4">
+      <div className="aspect-[3/2]">
+        <img
+          src={blog.image}
+          alt={blog.titre}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="p-6">
+        <p className="text-lg text-blue-500">{blog.titre}</p>
+        <p className="text-base">{blog.texte}</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/Common/Card";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/blog");
+        const blogsFromBackend = await response.json();
+        console.log(blogsFromBackend);
+        setBlogs(blogsFromBackend);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllBlogs();
+  }, []);
+  return (
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+        {blogs.map((blog) => (
+          <Card key={blog._id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
 }
