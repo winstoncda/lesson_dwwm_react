faa68600f8651cd047910e835c63444b03fb0265
winstoncda <olivier.pro.cda@gmail.com>
2025-09-04 15:26:01 +0200

    fix blog url

--- src/api/blog.api.js
+++ src/api/blog.api.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 export async function getBlogsFromApi() {
   try {
-    const response = await fetch(`${BASE_URL}/blog`);
+    const response = await fetch(`${BASE_URL}blog`);
     const blogsFromBackend = await response.json();
     return blogsFromBackend;
   } catch (error) {
